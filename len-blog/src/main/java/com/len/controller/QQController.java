e141d07d7d96e95afdf2926e2cb846c79c2464d1
90411 <904118787@qq.com>
2019-12-10 00:00:27 +0800

    dd

--- len-blog/src/main/java/com/len/controller/QQController.java
+++ len-blog/src/main/java/com/len/controller/QQController.java
@@ -9,6 +9,8 @@ import com.qq.connect.api.OpenID;
 import com.qq.connect.javabeans.AccessToken;
 import com.qq.connect.javabeans.qzone.UserInfoBean;
 import com.qq.connect.oauth.Oauth;
+import com.qq.connect.utils.QQConnectConfig;
+import com.qq.connect.utils.http.PostParameter;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * qq互联接入
@@ -43,14 +47,14 @@ public class QQController {
     @ResponseBody
     public Map<String, Object> checkSession(HttpServletRequest req, HttpServletResponse resp) {
         Map<String, Object> result = new HashMap<String, Object>();
-        HttpSession session =  req.getSession();
+        HttpSession session = req.getSession();
         //判断会话中是否存在用户的登录信息
         SysUser user = (SysUser) session.getAttribute("qqUser");
-        if(user != null){
-            result.put("userInfo",user);
-            result.put("login",true);
-        }else{
-            result.put("login",false);
+        if (user != null) {
+            result.put("userInfo", user);
+            result.put("login", true);
+        } else {
+            result.put("login", false);
         }
         return result;
     }
@@ -58,17 +62,17 @@ public class QQController {
 
     @GetMapping(value = "/qqLogin")
     public void qqLogin(HttpServletRequest req, HttpServletResponse resp) {
-        HttpSession session =  req.getSession();
+        HttpSession session = req.getSession();
         String url = req.getParameter("callBackUrl");
         //得到参数中跳转的url
-        log.info("得到的参数url:"+ url);
-        session.setAttribute("callBackUrl",url);
+        log.info("得到的参数url:" + url);
+        session.setAttribute("callBackUrl", url);
 
         //判断会话中是否存在用户的登录信息
         SysUser user = (SysUser) session.getAttribute("qqUser");
         try {
             resp.setContentType("text/html;charset=utf-8");
-            if(user != null){
+            if (user != null) {
                 resp.sendRedirect(url);
                 return;
             }
@@ -82,18 +86,37 @@ public class QQController {
         }
     }
 
+    private String[] extractionAuthCodeFromUrl(String url) throws QQConnectException {
+        if (url == null) {
+            throw new QQConnectException("you pass a null String object");
+        } else {
+            Matcher m = Pattern.compile("code=(\\w+)&state=(\\w+)&?").matcher(url);
+            String authCode = "";
+            String state = "";
+            if (m.find()) {
+                authCode = m.group(1);
+                state = m.group(2);
+            }
+
+            return new String[]{authCode, state};
+        }
+    }
+
     @GetMapping(value = "/callback")
-    public String callback(HttpServletRequest req,HttpServletResponse resp) {
+    public String callback(HttpServletRequest req, HttpServletResponse resp) {
         String url = "";
         try {
-            log.info("query:"+((HttpServletRequest)req).getQueryString());
+            log.info(req.getSession().getAttribute("qq_connect_state")+"==query:" + req.getQueryString());
+            String state = extractionAuthCodeFromUrl(req.getQueryString())[1];
+            req.getSession().setAttribute("qq_connect_state", state);
 
             HttpSession session = req.getSession();
             //得到参数中跳转的url
-            url =  (String) session.getAttribute("callBackUrl");
+            url = (String) session.getAttribute("callBackUrl");
 
             log.info("--------开始回调-----------" + url);
-            AccessToken accessTokenObj = new Oauth().getAccessTokenByRequest(req);
+            Oauth oauth = new Oauth();
+            AccessToken accessTokenObj = oauth.getAccessTokenByRequest(req);
             String accessToken = null;
             String openID = null;
             long tokenExpireIn = 0L;
@@ -129,12 +152,13 @@ public class QQController {
             user.setNickName(nickName);
             user.setGender(sex);
             wxUserService.insertSelective(user);
-            session.setAttribute("qqUser",user);
+            session.setAttribute("qqUser", user);
 
-            log.info("获得的信息"+ userInfoBean);
+            log.info("获得的信息" + userInfoBean);
         } catch (QQConnectException e) {
             e.printStackTrace();
         }
         return url;
     }
+
 }
